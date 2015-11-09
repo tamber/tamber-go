28bfd358b98f86fc44b29112e76b237cfa4af459
Alexander Robbins <alexi@tamber.com>
2015-11-08 16:51:46 -0800

    internalization fixes

--- actor/client.go
+++ actor/client.go
@@ -7,76 +7,77 @@ import (
 )
 
 type Engine struct {
-	*tamber.Engine
+	S   *tamber.SessionConfig
+	Key string
 }
 
 var object = "actor"
 
-func Create(params *ActorParams) (*Actor, error) {
+func Create(params *tamber.ActorParams) (*tamber.Actor, error) {
 	return getEngine().Create(params)
 }
 
-func (e Engine) Create(params *ActorParams) (*Actor, error) {
+func (e Engine) Create(params *tamber.ActorParams) (*tamber.Actor, error) {
 	body := &url.Values{}
 	params.AppendToBody(body)
-	actor := &Actor{}
+	actor := &tamber.Actor{}
 	var err error
 
-	if len(params.Id) > 0 && len(params.Behaviors) > 0 {
+	if len(params.Id) > 0 {
 		err = e.S.Call("POST", "", e.Key, object, "create", body, actor)
 	} else {
-		err = errors.New("Invalid actor params: either id or behaviors need to be set")
+		err = errors.New("Invalid actor params: id needs to be set")
 	}
 
 	return actor, err
 }
 
-func AddBehaviors(params *ActorParams) (*Actor, error) {
+func AddBehaviors(params *tamber.ActorParams) (*tamber.Actor, error) {
 	return getEngine().AddBehaviors(params)
 }
-func (e Engine) AddBehaviors(params *ActorParams) (*Actor, error) {
+func (e Engine) AddBehaviors(params *tamber.ActorParams) (*tamber.Actor, error) {
 	body := &url.Values{}
 	params.AppendToBody(body)
-	actor := &Actor{}
+	actor := &tamber.Actor{}
 	var err error
 
-	if len(params.Id) > 0 && lan(params.Behaviors) > 0 {
+	if len(params.Id) > 0 && len(*params.Behaviors) > 0 {
 		err = e.S.Call("POST", "", e.Key, object, "addBehaviors", body, actor)
 	} else {
 		err = errors.New("Invalid actor params: either id or behaviors need to be set")
 	}
-	return actor, nil
+	return actor, err
 }
 
-func RemoveBehaviors(params *ActorParams) (*Actor, error) {
+func RemoveBehaviors(params *tamber.ActorParams) (*tamber.Actor, error) {
 	return getEngine().RemoveBehaviors(params)
 }
 
-func (e Engine) RemoveBehaviors(params *ActorParams) (*Actor, error) {
+func (e Engine) RemoveBehaviors(params *tamber.ActorParams) (*tamber.Actor, error) {
 	body := &url.Values{}
 	params.AppendToBody(body)
-	actor := &Actor{}
+	actor := &tamber.Actor{}
 	var err error
 
-	if len(params.Id) > 0 && lan(params.Behaviors) > 0 {
+	if len(params.Id) > 0 && len(*params.Behaviors) > 0 {
 		err = e.S.Call("POST", "", e.Key, object, "removeBehaviors", body, actor)
 	} else {
 		err = errors.New("Invalid actor params: either id or behaviors need to be set")
 	}
-	return actor, nil
+	return actor, err
 }
 
-func Retrieve(params *ActorParams) (*Actor, error) {
+func Retrieve(params *tamber.ActorParams) (*tamber.Actor, error) {
 	return getEngine().Retrieve(params)
 }
 
-func (e Engine) Retrieve(params *ActorParams) (*Actor, error) {
+func (e Engine) Retrieve(params *tamber.ActorParams) (*tamber.Actor, error) {
 	body := &url.Values{}
 	params.AppendToBody(body)
-	actor := &Actor{}
+	actor := &tamber.Actor{}
 	var err error
 
-	if len(params.Id) > 0 && lan(params.Behaviors) > 0 {
+	if len(params.Id) > 0 {
 		err = e.S.Call("POST", "", e.Key, object, "retrieve", body, actor)
 	} else {
 		err = errors.New("Invalid actor params: id needs to be set")
@@ -85,17 +86,17 @@ func (e Engine) Retrieve(params *ActorParams) (*Actor, error) {
 	return actor, err
 }
 
-func Remove(params *ActorParams) (*Actor, error) {
+func Remove(params *tamber.ActorParams) (*tamber.Actor, error) {
 	return getEngine().Remove(params)
 }
 
-func (e Engine) Remove(params *ActorParams) (*Actor, error) {
+func (e Engine) Remove(params *tamber.ActorParams) (*tamber.Actor, error) {
 	body := &url.Values{}
 	params.AppendToBody(body)
-	actor := &Actor{}
+	actor := &tamber.Actor{}
 	var err error
 
-	if len(params.Id) > 0 && lan(params.Behaviors) > 0 {
+	if len(params.Id) > 0 {
 		err = e.S.Call("POST", "", e.Key, object, "remove", body, actor)
 	} else {
 		err = errors.New("Invalid actor params: id needs to be set")
@@ -105,5 +106,5 @@ func (e Engine) Remove(params *ActorParams) (*Actor, error) {
 }
 
 func getEngine() Engine {
-	return Engine{GetDefaultSessionConfig(), DefaultKey}
+	return Engine{tamber.GetDefaultSessionConfig(), tamber.DefaultKey}
 }
