9313559720f11d2db00f1f3e4cc6fe24ce9420d7
Alexander Robbins <alexi@tamber.com>
2017-04-24 13:11:20 -0700

    utils

--- utils.go
+++ utils.go
@@ -23,3 +23,19 @@ func Gzip(filepath string) error {
 	w.Close()
 	return nil
 }
+
+func StringPointer(v string) *string {
+	return &v
+}
+
+func Int64Pointer(v int64) *int64 {
+	return &v
+}
+
+func IntPointer(v int) *int {
+	return &v
+}
+
+func FloatPointer(v float64) *float64 {
+	return &v
+}
